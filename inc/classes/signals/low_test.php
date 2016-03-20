aaffbf618df54ee30c83f771a79ee294cb1ca0e6
Aran Reeks <cdtreeks@gmail.com>
2016-03-20 23:15:24 +0000

    Fixed a potential bug when low test could have been called on a short entry

--- inc/classes/signals/low_test.php
+++ inc/classes/signals/low_test.php
@@ -12,11 +12,17 @@ class low_test extends _signal {
      * @return bool
      */
     public static function isValidSignal(array $data, string $direction): bool {
+        if ($direction === 'short') {
+            trigger_error('Error: High test called for a short entry');
+
+            return false;
+        }
+
         /**@var avg_price_data $last_period*/
         $last_period = end($data);
         $last_direction = $last_period->getDirection();
 
-        if (($direction === 'long' && $last_direction === 'up') || ($direction === 'short' && $last_direction === 'down')) {
+        if ($direction === 'long' && ($last_direction === 'up' || $last_period === 'neutral')) {
             $body_bottom = ($last_period->close < $last_period->open ? $last_period->close : $last_period->open);
 
             $candle_top = abs($last_period->high - $body_bottom);
