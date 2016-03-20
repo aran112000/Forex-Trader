868871ee43b14abec21a36be463634bcaa99a438
Aran Reeks <cdtreeks@gmail.com>
2016-03-20 23:15:00 +0000

    Improved the high test signal to take into account price direction

--- inc/classes/signals/high_test.php
+++ inc/classes/signals/high_test.php
@@ -12,16 +12,24 @@ class high_test extends _signal {
      * @return bool
      */
     public static function isValidSignal(array $data, string $direction): bool {
-        $last_period = end($data);
+        if ($direction === 'long') {
+            trigger_error('Error: High test called for a short entry');
+            return false;
+        }
 
         /**@var avg_price_data $last_period */
-        $body_top = ($last_period->close > $last_period->open ? $last_period->close : $last_period->open);
+        $last_period = end($data);
+        $last_direction = $last_period->getDirection();
 
-        $candle_top = abs($last_period->high - $body_top);
-        $candle_bottom = abs($body_top - $last_period->low);
+        if ($direction === 'short' && ($last_direction === 'down' || $last_period === 'neutral')) {
+            $body_top = ($last_period->close > $last_period->open ? $last_period->close : $last_period->open);
 
-        if ($candle_top >= ($candle_bottom * 2)) {
-            return true;
+            $candle_top = abs($last_period->high - $body_top);
+            $candle_bottom = abs($body_top - $last_period->low);
+
+            if ($candle_top >= ($candle_bottom * 2)) {
+                return true;
+            }
         }
 
         return false;
