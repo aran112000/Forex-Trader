42cb3f8cde4b393440b7fc226f9ca66190340b4f
Aran Reeks <cdtreeks@gmail.com>
2016-03-27 11:54:18 +0100

    Removed direction check from the low test signals

--- inc/classes/signals/low_test.php
+++ inc/classes/signals/low_test.php
@@ -20,17 +20,14 @@ class low_test extends _signal {
 
         /**@var avg_price_data $last_period*/
         $last_period = end($data);
-        $last_direction = $last_period->getDirection();
 
-        if ($direction === 'long' && ($last_direction === 'up' || $last_period === 'neutral')) {
-            $body_bottom = ($last_period->close < $last_period->open ? $last_period->close : $last_period->open);
+        $body_bottom = ($last_period->close < $last_period->open ? $last_period->close : $last_period->open);
 
-            $candle_top = abs($last_period->high - $body_bottom);
-            $candle_bottom = abs($body_bottom - $last_period->low);
+        $candle_top = abs($last_period->high - $body_bottom);
+        $candle_bottom = abs($body_bottom - $last_period->low);
 
-            if ($candle_bottom >= ($candle_top * 2)) {
-                return true;
-            }
+        if ($candle_bottom >= ($candle_top * 2)) {
+            return true;
         }
 
         return false;
