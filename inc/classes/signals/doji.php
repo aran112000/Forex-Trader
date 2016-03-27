76e882071e2e25dc36ec161bb36dd25c233d538d
Aran Reeks <cdtreeks@gmail.com>
2016-03-27 14:33:50 +0100

    Further improvements to the DOJIs signal

--- inc/classes/signals/doji.php
+++ inc/classes/signals/doji.php
@@ -18,15 +18,21 @@ class doji extends _signal {
         $last_period = end($data);
 
         // As dojis need to be relatively small in size, the current period candle must be less than the period average
-        $last_candle_size = abs($last_period->high - $last_period->low);
-        if ($last_candle_size <= get::averageCandleSize($data, self::CANDLE_AVERAGE_PERIOD)) {
+        $candle_size = abs($last_period->high - $last_period->low);
+        $avg_candle_size = ((get::averageCandleSize($data, self::CANDLE_AVERAGE_PERIOD) / 3) * 2); // 2/3 of the AVG
 
+        if ($candle_size <= $avg_candle_size) {
             // Let's make sure our close price was somewhere within the middle 33% of the candle - This shows indecisive price movement
             $min_close = ((33.33 * ($last_period->high - $last_period->low) / 100) + $last_period->low);
             $max_close = ((66.66 * ($last_period->high - $last_period->low) / 100) + $last_period->low);
 
             if ($last_period->close >= $min_close && $last_period->close <= $max_close) {
-                return true;
+                $body_size = abs($last_period->open - $last_period->close);
+
+                $body_percentage = (($body_size / $candle_size) * 100);
+                if ($body_percentage <= 25) {
+                    return true;
+                }
             }
         }
 
