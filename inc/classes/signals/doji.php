73a473bd71ea2a124490a1d93bbc0b2776128f19
Aran Reeks <cdtreeks@gmail.com>
2016-03-27 11:53:01 +0100

    Removed market direction check (for the time being, until confirmed by IP what the best way to handle this would be). Also increased the DOJI top/bottom threshold

--- inc/classes/signals/doji.php
+++ inc/classes/signals/doji.php
@@ -17,7 +17,7 @@ class doji extends _signal {
         /**@var avg_price_data $last_period*/
         $last_period = end($data);
 
-        $market_direction = get::historicalPriceDirection($data);
+        /*$market_direction = get::historicalPriceDirection($data);
         $last_direction = $last_period->getDirection();
 
         // Bearish after a run up. Bullish after a run down
@@ -25,7 +25,7 @@ class doji extends _signal {
             ($market_direction === 'up' && ($last_direction === 'down' || $last_direction === 'neutral')) // Bullish
                 ||
             ($market_direction === 'down' && ($last_direction === 'up' || $last_direction === 'neutral')) // Bearish
-        ) {
+        ) {*/
 
             // As dojis need to be relatively small in size, the current period candle must be less than the period average
             $last_candle_size = abs($last_period->high - $last_period->low);
@@ -35,12 +35,12 @@ class doji extends _signal {
                 $top_size = abs($last_period->high - ($last_period->open > $last_period->close ? $last_period->open : $last_period->close));
                 $bottom_size = abs($last_period->low - ($last_period->open < $last_period->close ? $last_period->open : $last_period->close));
 
-                if ($body_size <= ($top_size * 1.1) && $body_size <= ($bottom_size * 1.1)) { // Must be < +10%
+                if ($body_size <= ($top_size * 1.5) && $body_size <= ($bottom_size * 1.5)) { // Must be < +50%
                     return true;
                 }
             }
 
-        }
+        //}
 
         return false;
     }
