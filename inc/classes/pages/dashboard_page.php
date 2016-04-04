d04913f641a24b58c8736bf7019c772ce1e50df1
Aran Reeks <cdtreeks@gmail.com>
2016-04-04 22:33:43 +0100

    Added a simple dashboard table demo - TODO populate from a new MySQL tabel

--- inc/classes/pages/dashboard_page.php
+++ inc/classes/pages/dashboard_page.php
@@ -5,27 +5,35 @@ class dashboard_page extends _page {
      * @return string
      */
     protected function getBody(): string {
-        return html_table::get([
-            [
-                'First name' => 'Aran',
-                'Surname' => 'Reeks',
-                'D.O.B.' => '26/05/1990',
-            ],
-            [
-                'First name' => 'Aran',
-                'D.O.B.' => '26/05/1990',
-            ],
-            [
-
-            ],
-        ]);
+        return '<h1>Dashboard</h1>
+        <p class="lead">Welcome back ' . user::get()['first_name'] . '</p>
+        <div class="row">
+            <div class="col-md-6">' . self::getActiveTrades() . '</div>
+            <div class="col-md-6">' . self::getRecentTrades() . '</div>
+        </div>';
     }
 
     public function getActiveTrades(): string {
-
+        return '<h2>Active Trades</h2>
+        ' . html_table::get([
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-danger">-£4.51</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+        ]);
     }
 
     public function getRecentTrades(int $limit = 5): string {
-
+        return '<h2>Recent Trades</h2>
+        ' . html_table::get([
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-danger">-£4.51</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+            ['Trade ID' => 12345679, 'Placed' => '01/04/2016 16:12', 'Pair' => 'GBP/USD', 'Status' => 'Active', 'Profit' => '<span class="text-success">£147.55</span>'],
+        ]);
     }
 }
